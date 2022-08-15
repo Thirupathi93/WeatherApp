1a60b4538c3cec52200d85963829500464ad5ada
deekshith685 <dixithdev1234@gmail.com>
2022-08-15 07:23:33 +0530

    API key fix

--- network/src/main/java/com/sample/network/remote/RemoteAPIService.kt
+++ network/src/main/java/com/sample/network/remote/RemoteAPIService.kt
@@ -1,6 +1,7 @@
 package com.sample.network.remote
 
 import com.sample.data.WeatherResponse
+import com.sample.network.BuildConfig
 import com.sample.network.utils.Constants
 import retrofit2.Response
 import retrofit2.http.GET
@@ -10,6 +11,6 @@ interface RemoteAPIService {
     @GET(Constants.WEATHER_FORECAST_ENDPOINT)
     suspend fun getWeatherReport(
         @Query("q") q: String,
-        @Query("appid") appId: String = "65d00499677e59496ca2f318eb68c049"
+        @Query("appid") appId: String = BuildConfig.API_KEY
     ): Response<WeatherResponse>
 }
